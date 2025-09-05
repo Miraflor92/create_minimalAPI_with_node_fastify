898bfa25d06cce316c7fc800ad8a6d0d66a68a63
Miraflor92 <florenciamensi@gmail.com>
2025-09-05 18:44:52 -0300

    Projeto de mini API utulizando boilerplate

---
+++ src/server.ts
@@ -0,0 +1,60 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+
+
+
+const server = fastify({logger: true});
+server.register(cors, {
+    origin: "*",
+});
+
+
+const teams = [
+    {id: 1, name: "Mercedes", country: "Germany"},
+    {id: 2, name: "Red Bull Racing", country: "Austria"},
+    {id: 3, name: "Ferrari", country: "Italy"},
+    {id: 4, name: "McLaren", country: "United Kingdom"},
+];
+
+const drivers = [
+    {id: 1, name: "Lewis Hamilton",  country: "United Kingdom"},
+    {id: 2, name: "Max Verstappen",  country: "Netherlands"},
+    {id: 3, name: "Charles Leclerc",  country: "Monaco"},
+    {id: 4, name: "Lando Norris",  country: "United Kingdom"},
+];
+
+server.get("/teams", async (request, response) => {
+  response.type("application/json").code(200);
+  
+    return {teams};
+});
+
+server.get("/drivers", async (request, response) => {
+  response.type("application/json").code(200);
+  
+    return {drivers};
+});
+
+
+interface DriverParams {
+    id: string;
+}
+
+server.get<{Params: DriverParams}>(
+    "/drivers/:id",
+     async (request, response) => {
+    const id = parseInt(request.params.id) ;
+    const driver = drivers.find((d) => d.id === id);
+    if (!driver) {
+        response.type("application/json").code(404);
+        return {error: "Driver not found"};
+    }else {
+        response.type("application/json").code(200);
+        return {driver};
+    }
+});
+
+
+server.listen({port: 3333},() => {
+    console.log("Server init");
+});
